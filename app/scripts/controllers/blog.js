8cff23e1064e80a2d85f52afb9b29376a39f93c7
magonbe <majogonbe@gmail.com>
2016-01-27 23:59:53 +0100

    1er Cambio

---
+++ app/scripts/controllers/blog.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name corlateApp.controller:BlogCtrl
+ * @description
+ * # BlogCtrl
+ * Controller of the corlateApp
+ */
+angular.module('corlateApp')
+  .controller('BlogCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name corlateApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the corlateApp
+ */
+angular.module('corlateApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
